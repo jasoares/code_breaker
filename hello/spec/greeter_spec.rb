dfe80097e9b6d539156bcecf4c7960f42e06b5ed
João Soares <jsoaresgeral@gmail.com>
2012-04-04 18:16:53 +0100

    added the cucumber greeter organized the file structure to fit inside the hello project

--- hello/spec/greeter_spec.rb
+++ hello/spec/greeter_spec.rb
@@ -1,3 +1,9 @@
+class RSpecGreeter
+  def greet
+    "Hello RSpec!"
+  end
+end
+
 describe "RSpec Greeter" do
   it "should say 'Hello RSpec!' when it receives greet() message" do
     greeter = RSpecGreeter.new
